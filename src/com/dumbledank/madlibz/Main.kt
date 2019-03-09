87cf1c21ed8a7796332f2682545ddbf1c028959d
briercliffe <matt.briercliffe@gmail.com>
2019-03-09 17:09:23 -0600

    Convert to AWS Lamdas (#1)
    * Refactor to be ran via aws lambdas

--- src/com/dumbledank/madlibz/Main.kt
+++
@@ -1,9 +0,0 @@
-package com.dumbledank.madlibz
-
-import io.ktor.server.engine.commandLineEnvironment
-import io.ktor.server.engine.embeddedServer
-import io.ktor.server.netty.Netty
-
-fun main(args: Array<String>) {
-    embeddedServer(Netty, commandLineEnvironment(args)).start()
-}
